c8eec1ba0afc0c33e4db593ce8ce5c89fd883334
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-03-14 10:22:57 +0100

    updated to version 2020_03_08_1332

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url = 'https://openxcom.org/git_builds/openxcom_git_master_2020_02_10_0926.zip'
+$url = 'https://openxcom.org/git_builds/openxcom_git_master_2020_03_08_1332.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Join-Path $ENV:LocalAppData Programs)"
   url           = $url
   softwareName  = 'xcom*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '4D239A38AC3C3BC8A1A0569E3A700E80C426D103F6FFE5D5F87BD1EBC99616D9'
+  checksum      = '3199A40BCA8DCA060D54DD4263C0DF29B753DAAC91F5BC1ED9C6A7C459F7D628'
   checksumType    = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
