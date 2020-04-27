b6157cbe90991ce489e13c0d26cbf67ec1443804
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-04-27 18:39:20 +0200

    updated to version 2020_04_26_0030

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,28 +1,23 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url = 'https://openxcom.org/git_builds/openxcom_git_master_2020_03_08_1332.zip'
+$url = 'https://openxcom.org/git_builds/openxcom_git_master_2020_04_26_0030.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Join-Path $ENV:LocalAppData Programs)"
   url           = $url
   softwareName  = 'xcom*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '3199A40BCA8DCA060D54DD4263C0DF29B753DAAC91F5BC1ED9C6A7C459F7D628'
+  checksum      = '01A3F5B8EFE0915AE2B560E56A7446F3BB3F0718F126DD69C10108358C44D7D7'
   checksumType    = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
 ## StartMenu
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir $GameName).lnk" `
-  -TargetPath "$(Join-Path $installLocation $GameNameLow).exe" `
+  -TargetPath "$(Join-Path $installLocation $GameName.toLower()).exe" `
   -WorkingDirectory "$installLocation"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir ReadMe.lnk)" `
   -TargetPath "$(Join-Path $installLocation README.md)"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'Release Notes.lnk')" `
   -TargetPath "$(Join-Path $installLocation CHANGELOG.txt)"
-
-## Desktop
-Install-ChocolateyShortcut -ShortcutFilePath "$shortcutPath" `
-  -TargetPath "$(Join-Path $installLocation $GameNameLow).exe" `
-  -WorkingDirectory "$installLocation"
--- tools/commonEnv.ps1
+++ tools/commonEnv.ps1
@@ -1,6 +1,5 @@
 $GameName = 'OpenXCOM'
-$GameNameLow = $GameName.toLower()
 
-$installLocation = [IO.Path]::Combine($ENV:LocalAppData, 'Programs', $GameNameLow)
+$installLocation = [IO.Path]::Combine($ENV:LocalAppData, 'Programs', $GameName.toLower())
 $shortcutPath = [IO.Path]::Combine($ENV:UserProfile, 'Desktop', "$GameName.lnk")
 $startMenuDir = [IO.Path]::Combine($ENV:AppData, 'Microsoft\Windows\Start Menu\Programs', "$GameName")
