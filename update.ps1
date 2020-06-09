2c0020bdaef4cf3adf4adaf5791b1deb1718ad9f
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-09 18:41:52 +0200

    minor update update script

--- update.ps1
+++ update.ps1
@@ -3,29 +3,27 @@ import-module au
 $domain   = 'https://openxcom.org'
 $releases = "$domain/git-builds"
 
-# https://openxcom.org/git_builds/openxcom_git_master_2020_06_05_0810.zip
-
 function global:au_SearchReplace {
-  @{
+  @{ 
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^\s*\$url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"   	= "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      # "(?i)(^\s*checksum\s*=\s*)('.*')"   	= "`$1'$($Latest.Checksum32)'"
+      # "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
-    ".\openxcom.nuspec" = @{
-      "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
-    }
+    # ".\openxcom.nuspec" = @{
+    #   "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
+    # }
   }
 }
 
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
-     $regex   = '.zip$'
-     $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href #2
-     $token = $url -split 'openxcom_git_master_' | select -First 1 -Skip 1 #3
-     $raw_version = $token -split '.zip' | select -Last 1 -Skip 1 #3
-	 $version = $raw_version -replace '_','.'
-     return @{ Version = $version; URL32 = $url }
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
+  $regex   = '.zip$'
+  $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href #2
+  $token = $url -split 'openxcom_git_master_' | select -First 1 -Skip 1 #3
+  $raw_version = $token -split '.zip' | select -Last 1 -Skip 1 #3
+  $version = $raw_version -replace '_','.'
+  return @{ Version = $version; URL32 = $url }
 }
 
 update -ChecksumFor 32
