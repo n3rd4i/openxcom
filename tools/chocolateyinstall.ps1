193a1673bbe7d0c6c9f4c077fb9815ef191bdfc3
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-14 20:48:26 +0200

    updated to version 2020.06.14.1036

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,9 +5,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Join-Path $ENV:LocalAppData Programs)"
-  url           = 'https://openxcom.org/git_builds/openxcom_git_master_2020_06_08_1453.zip'
-  softwareName  = 'xcom*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = 'd2edb00cc4782c44babbb6287afe033f373b79ce7cb2cec659b3a4993e87c126'
+  url           = 'https://openxcom.org/git_builds/openxcom_git_master_2020_06_14_1036.zip'
+  checksum      = 'fd3e693901a0536bfc55de9f4e1d05f912b9e70d6412a35f7c0a3af886cef782'
   checksumType    = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
@@ -16,6 +15,8 @@ Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir $GameName).lnk" `
   -TargetPath "$(Join-Path $installLocation $GameName.toLower()).exe" `
   -WorkingDirectory "$installLocation"
+
+## Docs
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir ReadMe.lnk)" `
   -TargetPath "$(Join-Path $installLocation README.md)"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'Release Notes.lnk')" `
--- tools/commonEnv.ps1
+++ tools/commonEnv.ps1
@@ -1,5 +1,4 @@
 $GameName = 'OpenXCOM'
 
 $installLocation = [IO.Path]::Combine($ENV:LocalAppData, 'Programs', $GameName.toLower())
-$shortcutPath = [IO.Path]::Combine($ENV:UserProfile, 'Desktop', "$GameName.lnk")
 $startMenuDir = [IO.Path]::Combine($ENV:AppData, 'Microsoft\Windows\Start Menu\Programs', "$GameName")
