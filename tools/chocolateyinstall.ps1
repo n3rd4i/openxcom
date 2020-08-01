3acdc23ef324d804b98ebe35b1437e48d7c54052
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-08-01 15:34:08 +0200

    updated to version 2020.07.31.0839

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Join-Path $ENV:LocalAppData Programs)"
-  url           = 'https://openxcom.org/git_builds/openxcom_git_master_2020_07_24_1517.zip'
-  checksum      = '6cc434d5e60c006e9f4ef2aedda55359c8a6a096cd72f62bc66fa8f0c3059c22'
+  url           = 'https://openxcom.org/git_builds/openxcom_git_master_2020_07_31_0839.zip'
+  checksum      = '34b4a65560764ee005cf9c0bf6084188c0a6a7e6eb54d6a40fe3d2e29a7b3f48'
   checksumType    = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
